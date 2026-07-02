8444ac2d251f0d79d25b14afa90f5f61b4d43a98
AbrMac <abraarjh@gmail.com>
2026-07-02 11:20:29 +0700

    Initial Commit

---
+++ Collab1/Collab1App.swift
@@ -0,0 +1,17 @@
+//
+//  Collab1App.swift
+//  Collab1
+//
+//  Created by abr on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Collab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Collab1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Collab1
+//
+//  Created by abr on 02/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
